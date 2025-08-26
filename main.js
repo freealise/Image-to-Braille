27f834ca54e1704d8d30de834b90ef1258f2831f
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-08-26 22:17:01 +0000

    Save main.js.

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ function initUI() {
 	setUIElement('#inverted', settings.inverted).onchange = (e) => {settings.inverted = e.target.checked; r()};
 	setUIElement('#dithering', settings.dithering).onchange = (e) => {settings.dithering = e.target.checked; r()};
 	setUIElement('#monospace', settings.monospace).onchange = (e) => {settings.monospace = e.target.checked; r()};
-	setUIElement('#spacing', settings.spacing).onchange = (e) => {settings.monospace = e.target.checked; r()};
+	setUIElement('#spacing', settings.spacing).onchange = (e) => {settings.spacing = e.target.checked; r()};
 
 	document.querySelector('#greyscale_mode').onchange = (e) => {
 		settings.greyscale_mode = e.target.value;
